6f091580a5df662075c5dcd43b9c285c0d4cb394
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-02-07 12:26:17 +0530

    palindrome triangle

---
+++ palindrometriangle.py
@@ -0,0 +1,2 @@
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print((10**i//9)**2) #the output is basically the square of quotient of (10^i/9)
